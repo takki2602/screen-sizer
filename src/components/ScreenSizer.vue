0db4805b471fb55f252b7503eea87fa19aa2cfd2
tom.pauli <tom.pauli@gollmann.com>
2024-11-27 09:04:52 +0100

    add ScreenSizer and remove unused files

---
+++ src/components/ScreenSizer.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const outerSize = ref(window.outerWidth + " x " + window.outerHeight);
+const innerSize = ref(window.innerWidth + " x " + window.innerHeight);
+
+onMounted(() => {
+  window.addEventListener("resize", () => {
+    outerSize.value = window.outerWidth + " x " + window.outerHeight;
+    innerSize.value = window.innerWidth + " x " + window.innerHeight;
+  });
+
+  setInterval(() => {
+    outerSize.value = window.outerWidth + " x " + window.outerHeight;
+    innerSize.value = window.innerWidth + " x " + window.innerHeight;
+  }, 100);
+});
+</script>
+
+<template>
+  <div>
+    <div class="card">
+      <h1>Outer Size: {{ outerSize }}</h1>
+      <h1>Inner Size: {{ innerSize }}</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
